fa9ad71bab2e016648b9000451597de0f984b253
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-08-30 23:32:25 +0000

    Add table query to get final row and use it to increment further rows ids

--- app.py
+++ app.py
@@ -15,20 +15,22 @@ conn.execute("""CREATE TABLE if not exists FIGURE
             ST_FOOT        CHAR(5) NOT NULL,
             END_FOOT   CHAR(5) NOT NULL);""")
 
+# If they choose to add, use their input to populate an insert query
+final_row = conn.execute("SELECT * FROM FIGURE ORDER BY ID DESC LIMIT 1;").fetchone()
+print(f"The last id is {final_row[0]}")
+
 # Prompt the user for the action of their choice
 command = input("""What would you like to do?
       *add* new figure,
       *show* all figures,
       *seed* the database \n""")
 
-# If they choose to add, use their input to populate an insert query
-last_id = conn.execute("SELECT ID FROM table FIGURE BY column DESC LIMIT 1;")
-print(f"The last id is {last_id}")
+
+
 if command == "add":
     fig_id = input("Id: ")
     figure_name = input("Figure Name: ")
-    starting_alignment = input("Starting Alignment
-                               : ")
+    starting_alignment = input("Starting Alignment: ")
     ending_alignment = input("Ending Alignment: ")
     steps = input("Number of Steps: ")
     starting_foot = input("Starting Foot: ")
@@ -48,7 +50,7 @@ elif command == "show":
     print("Selection completed successfully.")
     conn.close()
 elif command == "seed":
-    conn.execute("INSERT into FIGURE VALUES (5,'Reverse Turn', 'DW', 'DC', 6, 'LF', 'RF')")
+    conn.execute(f"INSERT into FIGURE VALUES ({final_row[0] + 1},'Reverse Turn', 'DW', 'DC', 6, 'LF', 'RF')")
     conn.commit()
     print(conn.total_changes)
 else:
