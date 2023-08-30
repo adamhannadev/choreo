8537697a83bdc7ebc376321215a983cb0042e695
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-08-30 20:52:35 +0000

    Update the insert statement to correctly fill in values using input variables

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
-
 # This program takes figure input from the user and saves to the database.
 
 import sqlite3
 
 #  Connect to the 'test.db' database and create it if it doesn't exist.
 conn = sqlite3.connect('test.db')
-print("Opened database successfully.")
 
-# conn.execute("""CREATE TABLE FIGURE
-#              (ID INT PRIMARY KEY    NOT NULL,
-#              NAME       TEXT       NOT NULL,
-#              ST_ALIGN   TEXT    NOT NULL,
-#              END_ALIGN  TEXT    NOT NULL,
-#              STEPS      INT     NOT NULL,
-#              ST_FOOT        CHAR(5) NOT NULL,
-#              END_FOOT   CHAR(5) NOT NULL);""")
-# print("Created the Figures table.")
-# conn.close()
+conn.execute("""CREATE TABLE if not exists FIGURE
+            (ID INT PRIMARY KEY    NOT NULL,
+            NAME       TEXT       NOT NULL,
+            ST_ALIGN   TEXT    NOT NULL,
+            END_ALIGN  TEXT    NOT NULL,
+            STEPS      INT     NOT NULL,
+            ST_FOOT        CHAR(5) NOT NULL,
+            END_FOOT   CHAR(5) NOT NULL);""")
 
-# conn.execute("INSERT INTO FIGURE (ID,NAME,ST_ALIGN,END_ALIGN,STEPS,ST_FOOT,END_FOOT) \
-#              VALUES (1, 'Reverse Turn', 'DC', 'DW',6,'LF','RF')")
-# conn.commit()
-# print("Record created successfully.")
-# conn.close()
+command = input("""What would you like to do?
+      CREATE new figure,
+      SHOW all figures \n""")
+if command == "CREATE":
+    fig_id = input("Id: ")
+    figure_name = input("Figure Name: ")
+    starting_alignment = input("Starting Alignment: ")
+    ending_alignment = input("Ending Alignment: ")
+    steps = input("Number of Steps: ")
+    starting_foot = input("Starting Foot: ")
+    ending_foot = input("Ending Foot: ")
 
-curs = conn.execute("SELECT id, name, st_align from FIGURE")
-for row in curs:
-    print(f"ID: {row[0]}")
-    print(f"Name: {row[1]}")
-    print(f"Starting Alignment: {row[2]}", "\n")
-print("Selection completed successfully.")
-conn.close()
+    conn.execute("INSERT INTO FIGURE VALUES (?,?,?,?,?,?,?)", (fig_id, figure_name, starting_alignment, ending_alignment, steps, starting_foot, ending_foot))
+    conn.commit()
+    print("Record created successfully.")
+    conn.close()
+elif command == "SHOW":
+    curs = conn.execute("SELECT id, name, st_align from FIGURE")
+    for row in curs:
+        print(f"ID: {row[0]}")
+        print(f"Name: {row[1]}")
+        print(f"Starting Alignment: {row[2]}", "\n")
+    print("Selection completed successfully.")
+    conn.close()
+else:
+    print("Sorry, please choose from the available options.")
+exit()
+
+
+
+
+
     
