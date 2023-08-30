d84c819ce805687b6cac1f0f4f941693c2179585
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-08-30 23:17:40 +0000

    Add seeding capability

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import sqlite3
 #  Connect to the 'test.db' database and create it if it doesn't exist.
 conn = sqlite3.connect('test.db')
 
+print("Creating table if one doesn't exist.")
 conn.execute("""CREATE TABLE if not exists FIGURE
             (ID INT PRIMARY KEY    NOT NULL,
             NAME       TEXT       NOT NULL,
@@ -14,10 +15,15 @@ conn.execute("""CREATE TABLE if not exists FIGURE
             ST_FOOT        CHAR(5) NOT NULL,
             END_FOOT   CHAR(5) NOT NULL);""")
 
+# Prompt the user for the action of their choice
 command = input("""What would you like to do?
-      CREATE new figure,
-      SHOW all figures \n""")
-if command == "CREATE":
+      *add* new figure,
+      *show* all figures,
+      *seed* the database \n""")
+
+# If they choose to add, use their input to populate an insert query
+
+if command == "add":
     fig_id = input("Id: ")
     figure_name = input("Figure Name: ")
     starting_alignment = input("Starting Alignment: ")
@@ -28,17 +34,23 @@ if command == "CREATE":
 
     conn.execute("INSERT INTO FIGURE VALUES (?,?,?,?,?,?,?)", (fig_id, figure_name, starting_alignment, ending_alignment, steps, starting_foot, ending_foot))
     conn.commit()
-    print("Record created successfully.")
+    print("Record created successfully.", conn.total_changes)
     conn.close()
-elif command == "SHOW":
-    curs = conn.execute("SELECT id, name, st_align from FIGURE")
+# If they choose to show the table, print out all records
+elif command == "show":
+    curs = conn.execute("SELECT id, name, st_align, end_align, steps, st_foot, end_foot from FIGURE")
     for row in curs:
         print(f"ID: {row[0]}")
         print(f"Name: {row[1]}")
         print(f"Starting Alignment: {row[2]}", "\n")
     print("Selection completed successfully.")
     conn.close()
+elif command == "seed":
+    conn.execute("INSERT into FIGURE VALUES (5,'Reverse Turn', 'DW', 'DC', 6, 'LF', 'RF')")
+    conn.commit()
+    print(conn.total_changes)
 else:
+    conn.close()
     print("Sorry, please choose from the available options.")
 exit()
 
