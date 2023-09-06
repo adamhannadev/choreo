c60217fa9bbe30875e0baf6350492d8bdb21e9a8
adamhannadev <46306810+adamhannadev@users.noreply.github.com>
2023-09-06 20:47:09 +0000

    Replace if else statement with a dictionaru of command functions

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # This program takes figure input from the user and saves to the database.
-
 import sqlite3
 
 #  Connect to the 'test.db' database and create it if it doesn't exist.
@@ -17,16 +16,8 @@ conn.execute("""CREATE TABLE if not exists FIGURE
 # If they choose to add, use their input to populate an insert query
 final_row = conn.execute("SELECT * FROM FIGURE ORDER BY ID DESC LIMIT 1;").fetchone()
 
-# Prompt the user for the action of their choice
-command = input("""What would you like to do?
-      *add* new figure,
-      *show* all figures,
-      *seed* the database,
-      *choreo*graph a new routine \n""")
-
-
 # If they choose the add command, create a new record from data inputs
-if command == "add":
+def add():
     figure_name = input("Figure Name: ")
     starting_alignment = input("Starting Alignment: ")
     ending_alignment = input("Ending Alignment: ")
@@ -39,7 +30,7 @@ if command == "add":
     conn.close()
 
 # If they choose to show the table, print out all records
-elif command == "show":
+def show():
     curs = conn.execute("SELECT id, name, st_align, end_align, st_foot, end_foot from FIGURE")
     for row in curs:
         print(f"ID: {row[0]}")
@@ -49,13 +40,7 @@ elif command == "show":
     print("Selection completed successfully.")
     conn.close()
 
-# If they choose to seed the database, enter preset values
-elif command == "seed":
-    conn.execute(f"INSERT into FIGURE VALUES (1,'Reverse Turn', 'DW', 'DC', 'LF', 'RF')")
-    conn.commit()
-    print(conn.total_changes)
-
-elif command == "choreo":
+def choreo():
     curs = conn.execute("SELECT id, name, st_align, end_align, st_foot, end_foot from FIGURE").fetchall()
     # Begin with the first figure
     # Append a new figure to the routine if the preceding figure's ending alignment is the same as the following figure's starting alignment
@@ -64,10 +49,16 @@ elif command == "choreo":
     print(f"Precede is {precede}")
     possible_follows = conn.execute(f"SELECT * from FIGURE WHERE ST_ALIGN = '{precede[3]}' AND NOT ST_FOOT = '{precede[5]}'").fetchall()
     print(f"Possible follows are - {possible_follows}")
-else:
     conn.close()
     print("Sorry, please choose from the available options.")
-exit()
+
+# Prompt the user for the action of their choice
+command = input("""What would you like to do?
+      *add* new figure,
+      *show* all figures,
+      *choreo*graph a new routine \n""")
+action = {'add': add, 'show': show, 'choreo': choreo}
+action[command]()
 
 
 
